adfa2c26776e1bc256bcab78429b5b358ac1c300
Brandon Wright <brandon.w.wright4@gmail.com>
2021-04-27 00:25:27 -0700

    add user input object, cellValue

--- app.js
+++ app.js
@@ -10,9 +10,12 @@ const app = Vue.createApp({
   methods: {
       userInput() {
         // this.userList.push(this.userText);
-        const cellValue = {
-          
-        }
+        let cellValue = {};
+          cellValue.name = this.userText;
+          cellValue.id = this.userList + 1;
+          cellValue.drawn = false;
+          this.userList.push(cellValue);
+        console.log("the array of objects: " + cellValue);
         return this.userText = '';
       }
   }
