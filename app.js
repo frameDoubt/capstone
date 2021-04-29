20c1dab3c1005d9513c18ee2c71b0f9266037d02
Brandon Wright <brandon.w.wright4@gmail.com>
2021-04-29 00:19:38 -0700

    add function that randomly selects from a filtered list

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = Vue.createApp({
       placeholderText: 'Enter name in this field',
       userPrompt: "Please enter names below.",
       userText: '',
-      userList: [],
+      userList: [
+        { name: "Brandon", id: 1, status: true, }
+      ],
       myGitHubRepo: "https://github.com/frameDoubt/table-maker"
     };
   },
@@ -12,23 +14,21 @@ const app = Vue.createApp({
       userInput() {
         // this.userList.push(this.userText);
         if(this.userText.length > 0) {
-          let cellValue = {};
-          cellValue.name = this.userText;
-          cellValue.id = this.userList.length + 1;
-          cellValue.drawn = false;
+          let cellValue = {
+            name: this.userText,
+            id: this.userList.length + 1,
+            status: false
+          };
           this.userList.push(cellValue);
           return this.userText = '';
         }
       },
       randomSelection() {
-        const mpdUserList = this.userList.map((item) => {
-          if(!item.drawn) {
-            return item.id;
-          }
-        });
-        console.log(this.userList[0].drawn);
-        this.userList[mpdUserList[0] - 1].drawn = true;
-        console.log(this.userList[0].drawn, this.userList[1].drawn);
+        const mpdUserList = this.userList.filter((item) => {
+          return item.status === false;
+        })
+        console.log(mpdUserList)
+        console.log(Math.floor(Math.random() * mpdUserList.length));
       }
   }
 });
